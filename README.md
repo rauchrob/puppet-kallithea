e249000a319d9ecec0dc5ec9c49a356d1ccdb1c1
Robert Rauch <mail@robertrauch.de>
2015-07-08 15:46:20 +0200

    add fedora 19 and 21 beaker nodesets

--- README.md
+++ README.md
@@ -121,6 +121,7 @@ This module has been developed been successfully tested using Kallithea v0.2.1 o
 
 * CentOS/RHEL 6 and 7
 * Debian 7
+* Fedora 20
 * Ubuntu 12.04 and 14.04
 
 Support for CentOS/RHEL 6 and Debian 6 is planned for future releases.
--- spec/acceptance/kallithea_spec.rb
+++ spec/acceptance/kallithea_spec.rb
@@ -19,7 +19,7 @@ describe 'kallithea class' do
     describe service('kallithea') do
       it { is_expected.to be_running }
       # the following will fail if using simple init script on systems using upstart
-      #it { is_expected.to be_enabled }
+      it { is_expected.to be_enabled }
     end
 
     describe port(5000) do
@@ -29,6 +29,28 @@ describe 'kallithea class' do
     describe command('curl -I localhost:5000 2> /dev/null | head -n1') do
       its(:exit_status) { should eq 0 }
       its(:stdout) { should eq "HTTP/1.1 200 OK\n" }
+    end
+
+    describe command('puppet resource service kallithea ensure=stopped') do
+      its(:exit_status) { should eq 0 }
+    end
+    
+    describe port(5000) do
+      it { 
+        sleep(5)
+        should_not be_listening
+      }
+    end
+
+    describe command('puppet resource service kallithea ensure=running') do
+      its(:exit_status) { should eq 0 }
+    end
+    
+    describe port(5000) do
+      it {
+        sleep(10)
+        should be_listening
+      }
     end
 
   end
