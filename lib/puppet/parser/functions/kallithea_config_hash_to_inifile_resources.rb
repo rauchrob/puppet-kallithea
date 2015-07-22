96f32c07908494a4224ecdc06df1fc23c4fc967e
Robert Rauch <mail@robertrauch.de>
2015-07-23 00:53:16 +0200

    prototypical implementation of function for unpacking config_hash

---
+++ lib/puppet/parser/functions/kallithea_config_hash_to_inifile_resources.rb
@@ -0,0 +1,25 @@
+module Puppet::Parser::Functions
+
+  # TODO: documentation
+  newfunction(:kallithea_config_hash_to_inifile_resources, :type => :rvalue) do |args|
+    # TODO: do some data validation on config_hash:
+    #   * must be a hash of strings, whose values are string-valued hashes of strings
+    config_hash = args[0]
+    result = {}
+
+    config_hash.each_key do |section|
+      config_hash[section].each_key do |setting|
+        result["#{section}/#{setting}"] = {
+          'ensure'  => 'present',
+          'section' => section,
+          'setting' => setting,
+          'value'   => config_hash[section][setting],
+        }
+      end
+    end
+
+    return result
+  end
+
+end
+
