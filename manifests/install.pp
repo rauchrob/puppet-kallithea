5e786c43fd7bf953295bf62019fc726703168bb0
Robert Rauch <mail@robertrauch.de>
2015-07-23 08:22:41 +0200

    first implementation

--- manifests/install.pp
+++ manifests/install.pp
@@ -10,6 +10,7 @@ class kallithea::install (
   $manage_python    = $::kallithea::manage_python,
   $repo_root        = $::kallithea::repo_root,
   $service_provider = $::kallithea::params::service_provider,
+  $version          = $::kallithea::version,
 ) inherits kallithea::params {
 
   if $manage_git {
@@ -83,7 +84,10 @@ class kallithea::install (
   # (see https://pip.pypa.io/en/latest/reference/pip_install.html#controlling-setup-requires)
 
   kallithea::package { 'PasteScript': } ->
-  kallithea::package { 'kallithea': }
+
+  kallithea::package { 'kallithea':
+    version => $version,
+  }
 
   package { $::kallithea::params::packages:
     ensure => present,
--- manifests/package.pp
+++ manifests/package.pp
@@ -3,12 +3,19 @@
 # This type will install the python package named after the resource title into
 # Kallitheas virtualenv.
 #
-define kallithea::package {
+define kallithea::package (
+  $venv = $::kalllithea::install::venv,
+  $version = undef,
+) {
 
-  $venv = "${::kallithea::app_root}/venv"
+  if $version {
+    $pkgname = "${title}==${version}"
+  } else {
+    $pkgname = $title
+  }
 
   python::pip { "${title}@${venv}":
-    pkgname    => $title,
+    pkgname    => $pkgname,
     virtualenv => $venv,
     owner      => $::kallithea::app_user,
     require    => Exec["python_virtualenv_${venv}"],
