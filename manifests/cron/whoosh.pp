f643816e1bed2d71918765ff81c55cf30b003ce5
Robert Rauch <mail@robertrauch.de>
2015-09-30 21:24:10 +0200

    initial implementation of kallithea::cron::whoosh

---
+++ manifests/cron/whoosh.pp
@@ -0,0 +1,24 @@
+# == Class kallithea::cron::whoosh
+#
+# This class is meant to be called from kallithea.  It creates a cronjob for
+# generating/refreshing the search index for Kallitheas Whoosh powered full
+# text search.
+#
+class kallithea::cron::whoosh (
+  $python_venv = $kallithea::install::venv,
+  $config_file = $kallithea::config::config_file,
+  $cron_params = {},
+) {
+
+  $cron_command = "${python_venv}/bin/paster make-index ${config_file}"
+
+  $cron_default_params = {
+    user    => $kallithea::app_user,
+    command => $cron_command,
+    minute  => 5,
+  }
+
+  $cron_resources_hash = { 'regenerate Kallitheas whoosh index' => $cron_params }
+  create_resources('cron', $cron_resources_hash, $cron_default_params)
+
+}
