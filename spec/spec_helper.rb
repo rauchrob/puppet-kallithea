06ee326cf332b4452c29a51248ab4cd11daaf9e5
Robert Rauch <mail@robertrauch.de>
2015-12-07 10:46:11 +0100

    workaround for stankevich/python #262

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 include RspecPuppetFacts
+
+RSpec.configure do |c|
+  c.default_facts = { :virtualenv_version => '13.1.2' }
+end
+
