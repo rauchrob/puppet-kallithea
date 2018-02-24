452fa3282e5cd20f5dc93633990b5aee199c2d2a
Robert Rauch <mail@robertrauch.de>
2018-02-25 00:58:52 +0100

    tiny refactoring

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'rspec-puppet-facts'
 include RspecPuppetFacts
 
 RSpec.configure do |c|
+    c.after(:suite) do RSpec::Puppet::Coverage.report!
     c.default_facts = {
       :virtualenv_version => '13.1.2',
       :osfamily => 'Debian',
